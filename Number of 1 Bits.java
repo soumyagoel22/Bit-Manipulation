717573f01cf830fa2180db0342c6902eadee0bc8
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-11 19:49:11 +0530

    Create Number of 1 Bits.java

---
+++ Number of 1 Bits.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int numSetBits(long a) {
+        long rem;
+        int c=0;
+        while(a!=0)
+        {
+            rem=a%2;
+            if(rem==1)
+            c++;
+            a=a/2;
+        }
+        return c;
+    }
+}
