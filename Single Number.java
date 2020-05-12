f751079679c0e18e9592e54b2d6fa966594093ee
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-12 18:21:06 +0530

    Create Single Number.java

---
+++ Single Number.java
@@ -0,0 +1,13 @@
+public class Solution {
+    // DO NOT MODIFY THE LIST. IT IS READ ONLY
+    public int singleNumber(final List<Integer> a) {
+    int a1,i,n;
+    n=a.size();
+    a1=a.get(0);
+    for(i=1;i<n;i++)
+    {
+        a1 ^= a.get(i);
+    }
+    return a1;
+    }
+}
