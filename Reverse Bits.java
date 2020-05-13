d349f252758d9778a2b419deab33caf17289310c
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-05-13 19:31:48 +0530

    Create Reverse Bits.java

---
+++ Reverse Bits.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public long reverse(long a) {
+          int i=31;
+          long a1=0,rem;
+          while(a!=0 && i>=0)
+          {
+              rem = a%2;
+              if(rem==1)
+              a1 = a1 + (long)(Math.pow(2,i));
+              a=a/2;
+              i--;
+          }
+          return a1;
+    }
+}
