a18707bcc9b430c36d111717b3c47ee42f363b3c
Soumya Goel <34788632+soumyagoel22@users.noreply.github.com>
2020-06-11 22:38:53 +0530

    Create Divide Integers.java

---
+++ Divide Integers.java
@@ -0,0 +1,34 @@
+public class Solution {
+    public int divide(int A, int B) {
+       int count=0,sign=1;
+       if(B==0 || (A==Integer.MIN_VALUE && B==-1))
+       return Integer.MAX_VALUE;
+       if(A==Integer.MIN_VALUE && B==1)
+       return Integer.MIN_VALUE;
+       long ab = A;
+       if(ab<0 && B<0)
+       {
+           ab=-ab;
+           B=-B;
+       }
+       else if(ab<0)
+       {
+           ab=-ab;
+           sign=-1;
+       }
+       else if(B<0)
+       {
+           B=-B;
+           sign=-1;
+       }
+       while(ab>=B)
+       {
+           ab=ab-B;
+           count++;
+       }
+       if(sign==-1)
+       return sign*count;
+       else
+       return count;
+    }
+}
